ebfb523d7bef8d7f73460952586d8613ac5a6367
ammarnasr <ammarnasraza@gmail.com>
2020-04-04 03:05:00 +0200

    Done++
    It is Now Rock Paper Scissors Lizard Spock

--- rps/app.js
+++ rps/app.js
@@ -7,11 +7,14 @@ const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissor_div = document.getElementById("scissor");
+const lizard_div = document.getElementById("lizard");
+const spock_div = document.getElementById("spock");
+
 
 function getComputerChoice(){
 
-    const choices = ['rock' , 'paper' , 'scissor'];
-    const randomNumber = Math.floor(Math.random()*3);
+    const choices = ['rock' , 'paper' , 'scissor' , "lizard" , "spock"];
+    const randomNumber = Math.floor(Math.random()*5);
     return choices[randomNumber];
 
 }
@@ -24,7 +27,7 @@ function win(userChoice , computerChoice){
     userScore++;
     userScore_span.innerHTML = userScore;
     compScore_span.innerHTML = compScore;
-    result_p.innerHTML = `${userChoice}${smallUserWord} beats ${computerChoice}${smallCompWord}. YOU WIN!`;
+    result_p.innerHTML = `${userChoice}${smallUserWord} VAPORIZES ${computerChoice}${smallCompWord}. YOU WIN!`;
     userChoice_div.classList.add("green-glow");
     setTimeout(() => userChoice_div.classList.remove("green-glow") , 500);
 
@@ -61,13 +64,22 @@ function game(userChoice){
     const computerChoice = getComputerChoice();
     switch (userChoice + computerChoice){
         case "rockscissor":
+        case "rocklizard":
         case "paperrock":
+        case "paperspock":
         case "scissorpaper":
+        case "scissorlizard":
+        case "lizardpaper":
+        case "lizardspock":
+        case "spockrock":
+        case "spockscissor":
             win(userChoice , computerChoice);
             break;
         case "rockrock":
         case "paperpaper":
         case "scissorscissor":
+        case "lizardlizard":
+        case "spockspock":
             draw(userChoice, computerChoice);
             break;
         default:
@@ -79,17 +91,11 @@ function game(userChoice){
 function main(){
 
 
-    rock_div.addEventListener('click' , function(){
-        game("rock");
-    })
-
-    paper_div.addEventListener('click' , function(){
-        game("paper");
-    })
-
-    scissor_div.addEventListener('click' , function(){
-        game("scissor");
-    })
+    rock_div.addEventListener('click' , () => game("rock") );
+    paper_div.addEventListener('click' ,() => game("paper"));
+    scissor_div.addEventListener('click' ,()=> game("scissor"));
+    lizard_div.addEventListener('click' ,()=> game("lizard"));
+    spock_div.addEventListener('click' ,()=> game("spock"));
 
 }
 
