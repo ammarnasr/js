e9f6b9c917653bf6a38a1b5f8ca66c652c637f38
ammarnasr <ammarnasraza@gmail.com>
2020-04-16 19:45:05 +0200

    Finally Started :)
    a small fade in animation of first element

--- ScrollMagicUniteOne/js/main.js
+++ ScrollMagicUniteOne/js/main.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
 
-	
+    //intit scroll magic
+    var controller = new ScrollMagic.Controller();
+
+    //build a scene to play the animation in
+    var ourScene = new ScrollMagic.Scene({
+        triggerElement : '#project01'
+    })
+    .setClassToggle('#project01' , 'fade-in')// add class .fade-in to element with id #project01
+    .addIndicators({
+        name : 'fade project01 scene',
+        colorTrigger : 'black',
+        indent : 200 , 
+        colorStart : '#756C695'
+    })// from the add indicator pligin
+    .addTo(controller)
 
 });
 
