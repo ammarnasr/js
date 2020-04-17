1c7081a28febd61687bb51c2dd1e41e56f6ce009
ammarnasr <ammarnasraza@gmail.com>
2020-04-17 23:52:37 +0200

    added pinnig to intro

--- ScrollMagicUniteOne/js/main.js
+++ ScrollMagicUniteOne/js/main.js
@@ -3,6 +3,24 @@ $(document).ready(function(){
     //intit scroll magic
     var controller = new ScrollMagic.Controller();
 
+    //pin intro scene
+    var pinIntroScene = new ScrollMagic.Scene({
+        triggerElement : '#intro',
+        triggerHook : 0,
+        duration : '30%'
+    })
+    .setPin('#intro' , {pushFollowers : false})
+    .addTo(controller)
+    
+    //pin again
+    var pinIntroScene2 = new ScrollMagic.Scene({
+        triggerElement : '#project01',
+        triggerHook : 0.4
+    })
+    .setPin('#intro' , {pushFollowers : false})
+    .addTo(controller)
+    
+
     //build a scene to play the animation in
     // loop through each element with class .project
     $('.project').each(function(){
